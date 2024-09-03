1385a2758c16d90312c991c985c8d96b1f915493
gpessoni <pessonigs@gmail.com>
2024-09-03 00:45:13 -0300

    🗃️ raw: Update the main page from project and made complete responsive from mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import { Button } from "primereact/button";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "primeicons/primeicons.css";
 import "./App.css";
+import { Dropdown } from "primereact/dropdown";
+import { InputNumber } from "primereact/inputnumber";
+import { Dialog } from "primereact/dialog";
+import { Toast } from "primereact/toast";
 
 interface Match {
   player1Id: number;
@@ -31,6 +35,8 @@ interface TeamStats {
 }
 interface Statistics {
   topScorerId: number | null;
+  topScorerGoals: number | null;
+  bestDefenseConceded: number | null;
   bestDefenseId: number | null;
   highestGoalMatch: {
     player1Id: number;
@@ -47,30 +53,65 @@ interface Statistics {
 function App() {
   const [tournament, setTournament] = useState<Tournament | null>(null);
   const [loading, setLoading] = useState(false);
+  const [dialogVisible, setDialogVisible] = useState(false);
+  const [tournamentType, setTournamentType] = useState("GROUP");
+  const [numberOfTeams, setNumberOfTeams] = useState(4);
+
+  const toast = useRef<Toast>(null);
+
+  const tournamentTypes = [
+    { label: "Grupo", value: "GROUP" },
+    { label: "Eliminatório", value: "ELIMINATORY" },
+  ];
 
   const generateTournament = async () => {
     setLoading(true);
     try {
+      if (tournamentType === "ELIMINATORY" && !isPowerOfTwo(numberOfTeams)) {
+        throw new Error(
+          "A quantidade de times deve ser uma potência de 2 para torneios eliminatórios."
+        );
+      }
+
+      const adjustedNumberOfTeams =
+        tournamentType === "ELIMINATORY"
+          ? Math.pow(2, Math.ceil(Math.log2(numberOfTeams)))
+          : numberOfTeams;
+
       const response = await axios.post<Tournament>(
         "http://localhost:3333/tournaments/generate",
         {
-          numberOfPlayers: 4,
-          tournamentType: "GROUP",
+          numberOfPlayers: adjustedNumberOfTeams,
+          tournamentType,
         }
       );
       setTournament(response.data);
-    } catch (error) {
+      setDialogVisible(false);
+    } catch (error: any) {
       console.error("Erro ao gerar torneio:", error);
+      if (toast.current) {
+        toast.current.show({
+          severity: "error",
+          summary: "Erro",
+          detail: error.message,
+        });
+      }
     } finally {
       setLoading(false);
     }
+  };
+
+  const isPowerOfTwo = (n: number): boolean => {
+    return n > 0 && (n & (n - 1)) === 0;
   };
 
   const calculateStatistics = (): Statistics => {
     if (!tournament) {
       return {
         topScorerId: null,
+        topScorerGoals: 0,
         bestDefenseId: null,
+        bestDefenseConceded: 0,
         highestGoalMatch: null,
         saldoGols: [],
       };
@@ -85,7 +126,6 @@ function App() {
       score2: number;
     } | null = null;
 
-    // Inicializa as estatísticas
     tournament.matches.forEach((match: Match) => {
       const { player1Id, player2Id, score1 = 0, score2 = 0 } = match;
 
@@ -139,16 +179,20 @@ function App() {
     let bestDefenseId: number | null = null;
     let maxGoals = -1;
     let minConceded = Infinity;
+    let topScorerGoals = 0;
+    let bestDefenseConceded = 0;
 
     Object.keys(totalScores).forEach((playerId) => {
       const { scored, conceded } = totalScores[parseInt(playerId)];
       if (scored > maxGoals) {
         maxGoals = scored;
         topScorerId = parseInt(playerId);
+        topScorerGoals = scored;
       }
       if (conceded < minConceded) {
         minConceded = conceded;
         bestDefenseId = parseInt(playerId);
+        bestDefenseConceded = conceded;
       }
     });
 
@@ -169,7 +213,9 @@ function App() {
 
     return {
       topScorerId,
+      topScorerGoals,
       bestDefenseId,
+      bestDefenseConceded,
       highestGoalMatch,
       saldoGols,
     };
@@ -177,7 +223,7 @@ function App() {
 
   const renderTournament = () => {
     if (!tournament) {
-      return <p>Ver torneios Antigos</p>;
+      return <p></p>;
     }
 
     switch (tournament.tournamentType) {
@@ -192,84 +238,102 @@ function App() {
 
   const renderGroup = () => {
     const { champion } = tournament!;
-    const { topScorerId, bestDefenseId, highestGoalMatch, saldoGols } =
-      calculateStatistics();
+    const {
+      topScorerId,
+      bestDefenseId,
+      highestGoalMatch,
+      saldoGols,
+      topScorerGoals,
+      bestDefenseConceded,
+    } = calculateStatistics();
 
     return (
-      <div className="tournament-container">
-        <h2>Torneio em Grupo</h2>
-        <div className="statistics">
-          <h3>Classificação</h3>
-          <DataTable
-            style={{
-              overflow: "auto",
-              border: "1px solid #ccc",
-            }}
-            value={saldoGols}
-            rows={10}
-            rowsPerPageOptions={[5, 10, 20]}
-          >
-            <Column sortable field="playerId" header="Jogador" />
-            <Column sortable field="pontos" header="Pontos" />
-            <Column sortable field="saldo" header="Saldo" />
-            <Column sortable field="vitorias" header="Vitórias" />
-            <Column sortable field="empates" header="Empates" />
-            <Column sortable field="derrotas" header="Derrotas" />
-          </DataTable>
-        </div>
-        <div className="matches">
-          <h3>Resultados dos Jogos</h3>
-          {tournament!.matches.map((match, index) => (
-            <div key={index} className="match">
-              <div className="team">
-                <div className="team-name">Jogador {match.player1Id}</div>
-                <div
-                  className={`score ${
-                    match.score1 && match.score2
-                      ? match.score1 > match.score2
-                        ? "winner"
-                        : match.score1 < match.score2
-                        ? "loser"
-                        : "draw"
-                      : ""
-                  }`}
-                >
-                  {match.score1 || 0}
+      <>
+        <Toast ref={toast} />
+        <hr />
+        <div className="tournament-container">
+          <h2>Torneio em Grupo</h2>
+          <div className="champion">
+            <h3>Campeão: Jogador {champion}</h3>
+          </div>
+          <div className="statistics">
+            <h3>Classificação</h3>
+            <DataTable
+              style={{
+                overflow: "auto",
+                border: "1px solid #ccc",
+              }}
+              value={saldoGols}
+              rows={10}
+              rowsPerPageOptions={[5, 10, 20]}
+            >
+              <Column sortable field="playerId" header="Jogador" />
+              <Column sortable field="pontos" header="Pontos" />
+              <Column sortable field="saldo" header="Saldo" />
+              <Column sortable field="vitorias" header="Vitórias" />
+              <Column sortable field="empates" header="Empates" />
+              <Column sortable field="derrotas" header="Derrotas" />
+            </DataTable>
+          </div>
+          <div className="matches">
+            <h3>Resultados dos Jogos</h3>
+            {tournament!.matches.map((match, index) => (
+              <div key={index} className="match">
+                <div className="team">
+                  <div className="team-name">Jogador {match.player1Id}</div>
+                  <div
+                    className={`score ${
+                      match.score1 && match.score2
+                        ? match.score1 > match.score2
+                          ? "winner"
+                          : match.score1 < match.score2
+                          ? "loser"
+                          : "draw"
+                        : ""
+                    }`}
+                  >
+                    {match.score1 || 0}
+                  </div>
+                </div>
+                <div className="team">
+                  <div className="team-name">Jogador {match.player2Id}</div>
+                  <div
+                    className={`score ${
+                      match.score1 && match.score2
+                        ? match.score2 > match.score1
+                          ? "winner"
+                          : match.score2 < match.score1
+                          ? "loser"
+                          : "draw"
+                        : ""
+                    }`}
+                  >
+                    {match.score2 || 0}
+                  </div>
                 </div>
               </div>
-              <div className="team">
-                <div className="team-name">Jogador {match.player2Id}</div>
-                <div
-                  className={`score ${
-                    match.score1 && match.score2
-                      ? match.score2 > match.score1
-                        ? "winner"
-                        : match.score2 < match.score1
-                        ? "loser"
-                        : "draw"
-                      : ""
-                  }`}
-                >
-                  {match.score2 || 0}
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-        <h3>Campeão: Jogador {champion}</h3>
-        <div className="statistics">
-          <h3>Estatísticas</h3>
-          <p>Artilheiro: Jogador {topScorerId}</p>
-          <p>Melhor Defesa: Jogador {bestDefenseId}</p>
-          {highestGoalMatch && (
+            ))}
+          </div>
+
+          <div className="statistics">
+            <h3>Estatísticas</h3>
             <p>
-              Maior Goleada: Jogador {highestGoalMatch.player1Id}{" "}
-              {highestGoalMatch.score1} x {highestGoalMatch.score2} Jogador{" "}
-              {highestGoalMatch.player2Id}
+              Artilheiro: Jogador {topScorerId} - {topScorerGoals} gols feitos
             </p>
-          )}
+            <p>
+              Melhor Defesa: Jogador {bestDefenseId} - {bestDefenseConceded}{" "}
+              gols sofridos
+            </p>
+            {highestGoalMatch && (
+              <p>
+                Maior Goleada: Jogador {highestGoalMatch.player1Id}
+                {highestGoalMatch.score1} x {highestGoalMatch.score2} Jogador{" "}
+                {highestGoalMatch.player2Id}
+              </p>
+            )}
+          </div>
         </div>
-      </div>
+      </>
     );
   };
 
@@ -286,101 +350,164 @@ function App() {
         {}
       );
 
-    const championId = tournament!.champion;
-    const { topScorerId, bestDefenseId, highestGoalMatch, saldoGols } =
-      calculateStatistics();
+    const champion = tournament!.champion;
+    const {
+      topScorerId,
+      bestDefenseId,
+      highestGoalMatch,
+      saldoGols,
+      bestDefenseConceded,
+      topScorerGoals,
+    } = calculateStatistics();
 
     return (
-      <div className="tournament-container">
-        <h2>Chaves Eliminatórias</h2>
-        {Object.keys(matchesByRound).map((round) => (
-          <div key={round} className="round">
-            <h3>Rodada {round}</h3>
-            {matchesByRound[parseInt(round)].map(
-              (match: Match, index: number) => (
-                <div key={index} className="match">
-                  <div className="team">
-                    <div className="team-name">Jogador {match.player1Id}</div>
-                    <div
-                      className={`score ${
-                        match.score1 && match.score2
-                          ? match.score1 > match.score2
-                            ? "winner"
-                            : match.score1 < match.score2
-                            ? "loser"
-                            : "draw"
-                          : ""
-                      }`}
-                    >
-                      {match.score1 || 0}
+      <>
+        <hr />
+        <div className="tournament-container">
+          <h2>Chaves Eliminatórias</h2>
+          <div className="champion">
+            <h3>Campeão: Jogador {champion}</h3>
+          </div>
+          {Object.keys(matchesByRound).map((round) => (
+            <div key={round} className="round">
+              <h3>Rodada {round}</h3>
+              {matchesByRound[parseInt(round)].map(
+                (match: Match, index: number) => (
+                  <div key={index} className="match">
+                    <div className="team">
+                      <div className="team-name">Jogador {match.player1Id}</div>
+                      <div
+                        className={`score ${
+                          match.score1 && match.score2
+                            ? match.score1 > match.score2
+                              ? "winner"
+                              : match.score1 < match.score2
+                              ? "loser"
+                              : "draw"
+                            : ""
+                        }`}
+                      >
+                        {match.score1 || 0}
+                      </div>
+                    </div>
+                    <div className="team">
+                      <div className="team-name">Jogador {match.player2Id}</div>
+                      <div
+                        className={`score ${
+                          match.score1 && match.score2
+                            ? match.score2 > match.score1
+                              ? "winner"
+                              : match.score2 < match.score1
+                              ? "loser"
+                              : "draw"
+                            : ""
+                        }`}
+                      >
+                        {match.score2 || 0}
+                      </div>
                     </div>
                   </div>
-                  <div className="team">
-                    <div className="team-name">Jogador {match.player2Id}</div>
-                    <div
-                      className={`score ${
-                        match.score1 && match.score2
-                          ? match.score2 > match.score1
-                            ? "winner"
-                            : match.score2 < match.score1
-                            ? "loser"
-                            : "draw"
-                          : ""
-                      }`}
-                    >
-                      {match.score2 || 0}
-                    </div>
-                  </div>
-                </div>
-              )
+                )
+              )}
+            </div>
+          ))}
+
+          <div className="statistics">
+            <h3>Estatísticas</h3>
+            <p>Artilheiro: Jogador {topScorerId} - {topScorerGoals} gols feitos</p>
+            <p>Melhor Defesa: Jogador {bestDefenseId} - {bestDefenseConceded} gols sofridos</p>
+            {highestGoalMatch && (
+              <p>
+                Maior Goleada: Jogador {highestGoalMatch.player1Id}{" "}
+                {highestGoalMatch.score1} x {highestGoalMatch.score2} Jogador{" "}
+                {highestGoalMatch.player2Id}
+              </p>
             )}
           </div>
-        ))}
-        <h3>Campeão: Jogador {championId}</h3>
-        <div className="statistics">
-          <h3>Estatísticas</h3>
-          <p>Artilheiro: Jogador {topScorerId}</p>
-          <p>Melhor Defesa: Jogador {bestDefenseId}</p>
-          {highestGoalMatch && (
-            <p>
-              Maior Goleada: Jogador {highestGoalMatch.player1Id}{" "}
-              {highestGoalMatch.score1} x {highestGoalMatch.score2} Jogador{" "}
-              {highestGoalMatch.player2Id}
-            </p>
-          )}
+          <div className="statistics">
+            <h4>Saldo de Gols</h4>
+            <DataTable
+              style={{
+                width: "100%",
+                overflow: "auto",
+                border: "1px solid #ccc",
+              }}
+              value={saldoGols}
+              rows={10}
+              rowsPerPageOptions={[5, 10, 20]}
+            >
+              <Column sortable field="playerId" header="Jogador" />
+              <Column sortable field="saldo" header="Saldo de Gols" />
+            </DataTable>
+          </div>
         </div>
-        <div className="statistics">
-          <h4>Saldo de Gols</h4>
-          <DataTable
-            style={{
-              width: "100%",
-              overflow: "auto",
-              border: "1px solid #ccc",
-            }}
-            value={saldoGols}
-            rows={10}
-            rowsPerPageOptions={[5, 10, 20]}
-          >
-            <Column sortable field="playerId" header="Jogador" />
-            <Column sortable field="saldo" header="Saldo de Gols" />
-          </DataTable>
-        </div>
-      </div>
+      </>
     );
   };
 
   return (
-    <div className="App">
-      <div className="container">
-        <h1>Gerador de Torneios</h1>
-        <Button
-          label="Gerar Torneio"
-          onClick={generateTournament}
-          loading={loading}
-        />
-        {renderTournament()}
+    <>
+      <div className="App">
+        <div className="container">
+          <Toast ref={toast} />
+          <h1>Gerador de Torneios</h1>
+          <Button
+            label="Gerar Torneio"
+            onClick={() => setDialogVisible(true)}
+            loading={loading}
+          />
+          <br /> <br />
+          <p>Ver torneios Antigos</p>
+          {renderTournament()}
+        </div>
       </div>
-    </div>
+      <Dialog
+        draggable={false}
+        header="Configurações do Torneio"
+        visible={dialogVisible}
+        style={{ width: "50vw" }}
+        modal
+        closable
+        closeIcon="pi pi-times"
+        closeOnEscape={true}
+        className="p-fluid"
+        onHide={() => setDialogVisible(false)}
+      >
+        <div className="p-fluid">
+          <div className="p-field">
+            <label htmlFor="tournamentType">Tipo de Torneio</label>
+            <Dropdown
+              id="tournamentType"
+              value={tournamentType}
+              options={tournamentTypes}
+              onChange={(e) => setTournamentType(e.value)}
+              optionLabel="label"
+              placeholder="Selecione o tipo de torneio"
+            />
+          </div>
+          <div className="p-field">
+            <label htmlFor="numberOfTeams">Número de Times</label>
+            <InputNumber
+              id="numberOfTeams"
+              value={numberOfTeams}
+              onValueChange={(e) => setNumberOfTeams(e.value || 4)}
+              min={2}
+              max={64}
+              step={1}
+            />
+          </div>
+          <Button
+            label="Gerar"
+            icon="pi pi-check"
+            className="p-button-success"
+            onClick={generateTournament}
+            style={{
+              marginTop: 20,
+            }}
+          />
+        </div>
+      </Dialog>
+    </>
   );
 }
 
